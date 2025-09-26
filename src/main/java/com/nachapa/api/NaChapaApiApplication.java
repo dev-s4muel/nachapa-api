c4bbd15e3cbbbd6e9eaa542269d90d1d4fd122b9
dev.s4muel <dev.s4muel@gmail.com>
2025-09-26 15:22:27 -0300

    feat: FIRST COMMIT. Base e Infra necessária para começar o projeto

---
+++ src/main/java/com/nachapa/api/NaChapaApiApplication.java
@@ -0,0 +1,13 @@
+package com.nachapa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaChapaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaChapaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nachapa/api/NaChapaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachapa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaChapaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
